5ed57d8cb00d81d14c5aa524dc62b87c86c806b5
estrella <estrellafriasmejia@gmail.com>
2026-02-24 09:11:42 -0400

    prueba

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port =process.env.port || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine", "ejs");
+
+app.get("/",(req,res) =>{
+    res.render("index")
+})
+app.get("/about",(req,res) =>{
+    res.render("about")
+})
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+})
