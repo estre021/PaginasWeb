53585d7ea33737ebf66e4b46f62116ea7ce4057a
estrella <estrellafriasmejia@gmail.com>
2026-03-02 17:18:11 -0400

    ultimo

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import {fileURLToPath} from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const port =process.env.port || 3000;
+const port =process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
