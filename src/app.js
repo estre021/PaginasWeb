1966699ca30f4e3e9c29b4f8ef133693a2d6942c
estrella <estrellafriasmejia@gmail.com>
2026-03-03 08:50:03 -0400

    Actualizado la carpeta                            </div>

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import {fileURLToPath} from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+console.log(__dirname);
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
